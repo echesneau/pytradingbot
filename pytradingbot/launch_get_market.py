6674c8e94f34f9ad45fe8b3f720d53347d4deb7f
Erwan Chesneau <erwanchesneau@gmail.com>
2023-01-14 09:37:34 +0100

    Launch script to get market

---
+++ pytradingbot/launch_get_market.py
@@ -0,0 +1,25 @@
+#!/bin/python3.6
+
+import argparse
+import os.path
+import sys.exit
+
+from pytradingbot.iolib.crypto_api import KrakenApiDev
+
+#============================
+#arguments
+#============================
+parser = argparse.ArgumentParser(prog="launch_get_market.py", \
+                                     description= "\n"\
+                                     "Written by E. CHESNEAU")
+parser.add_argument('--ifile', '-i', help='input xml file')
+
+args = parser.parse_args()
+
+if args.ifile is None or not os.path.isfile(args.ifile):
+    print(f"STOP: {args.ifile} is not a file")
+    sys.exit()
+
+api = KrakenApiDev(user="erwan", inputs=args.ifile)
+api.connect()
+api.run()
