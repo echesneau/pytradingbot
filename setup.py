490099bee1ab07896323999aa3aeef9fa95fc974
Erwan Chesneau <erwanchesneau@gmail.com>
2023-03-26 14:42:42 +0200

    update documentation and requirement

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+from version import __version__
 
 setup(
     name='pytradingbot',
-    version="0.0.2",
+    version=__version__,
     packages=find_packages(),
     include_package_data=True,
     python_requires=">=3.6",
